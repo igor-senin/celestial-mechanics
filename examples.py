03c2a882442f73a02462b94b5ff690b3fc6e2b2f
Sferf <Isach.Pasha@yandex.by>
2023-10-04 23:24:55 +0300

    add examples

---
+++ examples.py
@@ -0,0 +1,186 @@
+import graphics
+from graphics import CelestialBody
+
+from decimal import *
+from math import sqrt
+
+
+def case_3_bodies():
+    Coeff = 3.844 * 10**(9) / (graphics.Height / 2.0)
+
+    earth = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 2.0),
+                         Decimal(Coeff * graphics.Height / 2.0)],
+            velocity=[Decimal(-250.0), Decimal(150.0)],
+            weight=Decimal(5.9742 * 10**24),
+            radius=Decimal(6.378 * 10**6),
+            id=1,
+            colour=graphics.Blue,
+            visible_radius=40.0
+            )
+
+    moon = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 2.0),
+                         Decimal(0.0)],
+            velocity=[Decimal(300.0), Decimal(150.0)],
+            weight=earth.weight,
+            radius=Decimal(float(earth.radius)/10.0),
+            id=2,
+            colour=graphics.White,
+            visible_radius=20.0
+            )
+
+    mercury = CelestialBody(
+            coordinates=[Decimal(Coeff * 1500.0), Decimal(Coeff * 500.0)],
+            velocity=[Decimal(150.0), Decimal(-200.0)],
+            weight=earth.weight,
+            radius=Decimal(float(earth.radius)/10.0),
+            id=3,
+            colour=graphics.Red,
+            visible_radius = 25.0
+            )
+
+    return ([earth, moon, mercury], Coeff)
+
+def case_solar_system():
+    Coeff = 1.4959787 * 10**12 / (graphics.Height / 2.0)
+
+    sun = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 2.0),
+                         Decimal(Coeff * graphics.Height / 2.0)],
+            velocity=[Decimal(0.0), Decimal(0.0)],
+            weight=Decimal(1.98892 * 10**33),
+            radius=Decimal(6.378 * 10**6),
+            id=1,
+            colour=graphics.Yellow,
+            visible_radius=70.0
+            )
+
+    moon = CelestialBody(
+            coordinates=[Decimal(Coeff * 50), Decimal(Coeff * 50)],
+            velocity=[Decimal(1000.0), Decimal(200.0)],
+            weight=Decimal(7.36 * 10**22),
+            radius=Decimal(float(sun.radius)/10.0),
+            id=2,
+            colour=graphics.White,
+            visible_radius=20.0
+            )
+
+    mars = CelestialBody(
+            coordinates=[Decimal(Coeff * 0.0), Decimal(Coeff * 500.0)],
+            velocity=[Decimal(750.0), Decimal(300.0)],
+            weight=Decimal(6.39 * 10**23),
+            radius=Decimal(float(sun.radius)/10.0),
+            id=3,
+            colour=graphics.Red,
+            visible_radius = 25.0
+            )
+
+    earth = CelestialBody(
+            coordinates=[Decimal(Coeff * 500.0), Decimal(Coeff * 500.0)],
+            velocity=[Decimal(100000.0), Decimal(100000.0)],
+            weight=Decimal(5.9742 * 10**24),
+            radius=Decimal(float(sun.radius)/10.0),
+            id=4,
+            colour=graphics.Blue,
+            visible_radius = 30.0
+            )
+
+    return ([sun, earth, moon, mars], Coeff)
+
+
+def case_2_bodies():
+    Coeff = 3.844 * 10**(9) / (graphics.Height / 2.0)
+
+    earth = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 2.0),
+                         Decimal(Coeff * graphics.Height / 2.0)],
+            velocity=[Decimal(0), Decimal(0)],
+            weight=Decimal(5.9736 * 10**24),
+            radius=Decimal(6.378 * 10**6),
+            id=1,
+            colour=graphics.Blue,
+            visible_radius=20.0
+            )
+
+    moon = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 2.0), Decimal(Coeff * 0)],
+            velocity=[Decimal(-200), Decimal(0)],
+            weight=Decimal(7.3477 * 10**22),
+            radius=Decimal(earth.radius) / Decimal(10.0),
+            id=2,
+            colour=graphics.White,
+            visible_radius=20.0
+            )
+    print(Decimal(Coeff * graphics.Height / 2.0))
+    return ([earth, moon], Coeff)
+
+
+def case_2_bodies_parabola():
+    Coeff = 3.844 * 10**(9) / (graphics.Height / 2.0)
+
+    earth = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 4 * 3),
+                         Decimal(Coeff * graphics.Height / 3.0)],
+            velocity=[Decimal(-200), Decimal(0)],
+            weight=Decimal(5.9736 * 10**24),
+            radius=Decimal(6.378 * 10**6),
+            id=1,
+            colour=graphics.Blue,
+            visible_radius=20.0
+            )
+
+    moon = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 4),
+                         Decimal(Coeff * 2.0*graphics.Height/3.0)],
+            velocity=[Decimal(200), Decimal(0)],
+            weight=Decimal(5.9736 * 10**24),
+            radius=Decimal(earth.radius) / Decimal(10.0),
+            id=2,
+            colour=graphics.White,
+            visible_radius=20.0
+            )
+    print(Decimal(Coeff * graphics.Height / 2.0))
+    return ([earth, moon], Coeff)
+
+
+def case_3_bodies_triangle():
+    Coeff = 3.844 * 10**(9) / (graphics.Height / 2.0)
+
+    H = graphics.Height / 3.0
+    A = 2 * H / sqrt(3)
+
+    moon1 = CelestialBody(
+            coordinates=[Decimal(Coeff * graphics.Width / 2.0),
+                         Decimal(Coeff * graphics.Height / 3.0)],
+            velocity=[Decimal(-100), Decimal(0)],
+            weight=Decimal(9 * 10**23),
+            radius=Decimal(6.378 * 10**6),
+            id=1,
+            colour=graphics.Blue,
+            visible_radius=20.0
+            )
+
+    moon2 = CelestialBody(
+            coordinates=[Decimal(Coeff * (graphics.Width / 2.0 - A / 2.0)),
+                         Decimal(Coeff * 2 * graphics.Height / 3.0)],
+            velocity=[Decimal(50), Decimal(50 * sqrt(3.0))],
+            weight=moon1.weight,
+            radius=Decimal(6.378 * 10**6),
+            id=2,
+            colour=graphics.White,
+            visible_radius=20.0
+            )
+
+    moon3 = CelestialBody(
+            coordinates=[Decimal(Coeff * (graphics.Width / 2.0 + A / 2.0)),
+                         Decimal(Coeff * 2 * graphics.Height / 3.0)],
+            velocity=[Decimal(50), Decimal(-50 * sqrt(3.0))],
+            weight=moon1.weight,
+            radius=Decimal(6.378 * 10**6),
+            id=3,
+            colour=graphics.Red,
+            visible_radius=20.0
+            )
+    return ([moon1, moon2, moon3], Coeff)
+
